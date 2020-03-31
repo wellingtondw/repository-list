3165c313ad74e99b594414858d01d7e6217b1076
wellington <wellingtonslpbi@hotmail.com>
2020-03-31 08:11:22 -0300

    create style main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Title } from './styles';
-
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 export default function Main() {
   return (
-    <div>
-      <Title>Main</Title>
-    </div>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repossitórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -18,4 +18,14 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased !important;
   }
 
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif, Arial, Helvetica, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
 `;
